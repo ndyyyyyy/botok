dd4caf2eabd1859681af5d9e8588bd550e4a079c
ndyyyyyy <144023429+ndyyyyyy@users.noreply.github.com>
2023-09-07 21:42:27 +0200

    Update bot.py
    uj fix mukodik

--- bot.py
+++ bot.py
@@ -7,22 +7,33 @@ nev2 = input("Mi a második neved?--->")
 link = input("Írd ide a linket---->")
 x = nev[0].upper() + nev[1:].lower()
 y = nev2[0].upper() + nev2[1:].lower()
+link2 = link.lower()
 xy = x + " " + y
-xylink = link.lower() 
+xylink = " ---> " + link2
 d = "Nev -->  "
 
 
-with open("nevek.txt", "r+") as f:
+
+#def nevvissza(link, uzenet="Ez a link mar letezik, probald ujra!"):
+with open("nevek.txt", "r") as f:
     data = f.read()
-    f.write(d + xy +  " ---> " + xylink + "\n")
-    f.close()
+   # print(data)
+    temp = data.find(xy)
+    link33 = data[temp + len(xy) + 6: data.find("Nev", temp)]
+   # print(link33)
+    f.close()   
 
-
-
-
-
-
-with open(file, 'r') as f:
-	links = f.readlines()
-for i in links:
+if link2 == link33:
+    print("MAr haszanalt")
     
+else:
+  with open("nevek.txt","r+") as f:
+     data = f.read()
+     f.write(d + str(xy) + xylink + "\n")   
+     f.close()  
+
+    # getlink = link.readlines(link)
+    #if link in getlink:
+     #  nevvissza(print(uzenet))
+   # f.close()
+
