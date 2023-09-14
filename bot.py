1b232b71bf11ed1cf5a9e7c7dad3f29c7478358c
ndyyyyyy <144023429+ndyyyyyy@users.noreply.github.com>
2023-09-14 20:51:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,65 +2,72 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-nev = input("Mi az elso neved?---->")
-nev2 = input("Mi a második neved?--->")
-link = input("Írd ide a linket---->")
-x = nev[0].upper() + nev[1:].lower()
-y = nev2[0].upper() + nev2[1:].lower()
-link2 = link.lower()
-xy = x + " " + y
-xylink = " ---> " + link2
-d = "Nev -->  "
-
-
-
 
 with open("nevek.txt", "r") as f:
     data = f.read()
-    #temp1 = data.find(xylink)
-    #linklength = data[temp1 + len(link2)]
-   # print(linklength)
-
-    # print(data)
-    #temp = data.find(xy)
-    #link33 = data[temp + len(xy) + 6: data.find("Nev", temp)]
-    #d_finder = data.find(xy)
-    # print(link33)
-    f.close()   
-
-
-if xylink in data:
-    print("Ez a link sajnos már használva van!")
-    print("Próbálja újra")
+    f.close()
 
     
-else:
-  with open("nevek.txt","r+") as f:
+nev = input("Mi az elso neved?---->")
+if nev == "fk=%!876)=//=UVhH=(%!§dkdj78756)":
+  print("Admin")
+elif nev != "fk=%!876)=//=UVhH=(%!§dkdj78756)":
+ nev2 = input("Mi a második neved?--->")
+ link = input("Írd ide a linket---->")
+ x = nev[0].upper() + nev[1:].lower()
+ y = nev2[0].upper() + nev2[1:].lower()
+ link2 = link.lower()
+ xy = x + " " + y
+ xylink = " ---> " + link2
+ d = "Nev -->  "
+   
+
+ix = 0
+while ix < 999999999:
+  if xylink in data:
+    print("Ez a link sajnos már használva van!")
+    print("Próbálja újra!")
+    i + 1
+    
+  else:
+    with open("nevek.txt","r+") as f:
      data = f.read()
      f.write(d + str(xy) + xylink + "\n")   
      f.close()  
-     print("Sikeresen hozza adva")
+     print("Sikeresen hozza adva!")
+     break
 
-szabalyzat = "ect...ect...ect..."
 
 
-i=1
 
-while i < 35455559789789789789798789:
-    help = input('Kell segítség?- "Igen" vagy "Nem"-->')
-    if help.lower() =="igen":
-        
-        print(szabalyzat)
+i = 0
+
+szabályzat = "fffff"
+
+while i < 9999999999999999:
+   help = input('Kell segítség?- "igen" vagy "nem" vagy "adataim"--->')
+   if help.lower() =="igen":
+    print(szabályzat)
+    break
+   
+   if help.lower() =="adataim":
+      nev11 = input("Mi az elso neved?---->")
+      nev22 = input("Mi a második neved?--->")
+      xd = nev11[0].upper() + nev11[1:].lower()
+      yd = nev22[0].upper() + nev22[1:].lower()
+      xyd = xd + " " + yd
+      d = "Nev -->  "
+   
+      with open("nevek.txt", "r") as f:
+        data = f.read()
+        adataimm = data.find(xyd)
+        getdata = data[adataimm : len(xyd)]
+        print(getdata)
+        f.close()
         break
 
-    if help.lower() =="nem":
-        print("A program sikeresen lefutott!!")
-        break
-       
-
-    else:
-        i += 1
-        
-
-#Nev -->  F F ---> https://www.facebook.com/profile.php?id=100082492849171
-      
+   if help.lower() =="nem":
+    break
+   
+   else:
+    i += 1
