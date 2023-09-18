fe41c61c0bab22d9580e47f4e40e7d3c078e6297
ndyyyyyy <144023429+ndyyyyyy@users.noreply.github.com>
2023-09-18 21:55:10 +0200

    bot.py
    MUKODIK GECI

--- bot.py
+++ bot.py
@@ -13,22 +13,23 @@ if nev == "fk=%!876)=//=UVhH=(%!§dkdj78756)":
   print("Admin")
 elif nev != "fk=%!876)=//=UVhH=(%!§dkdj78756)":
  nev2 = input("Mi a második neved?--->")
- link = input("Írd ide a linket---->")
  x = nev[0].upper() + nev[1:].lower()
  y = nev2[0].upper() + nev2[1:].lower()
- link2 = link.lower()
  xy = x + " " + y
- xylink = " ---> " + link2
- d = "Nev -->  "
+ d = "Nev-->"
    
 
 ix = 0
 while ix < 999999999:
+  link = input("Írd ide a linket---->")
+  link2 = link.lower()
+  xylink = "--->" + link2
   if xylink in data:
     print("Ez a link sajnos már használva van!")
     print("Próbálja újra!")
-    i + 1
-    
+
+    ix + 1
+
   else:
     with open("nevek.txt","r+") as f:
      data = f.read()
@@ -56,18 +57,28 @@ while i < 9999999999999999:
       xd = nev11[0].upper() + nev11[1:].lower()
       yd = nev22[0].upper() + nev22[1:].lower()
       xyd = xd + " " + yd
-      d = "Nev -->  "
+      print(xyd)
+      d = "Nev-->"
    
       with open("nevek.txt", "r") as f:
         data = f.read()
+       # print(data)
         adataimm = data.find(xyd)
-        getdata = data[adataimm : len(xyd)]
+        print(len(xyd))
+        print(adataimm)
+        getdata = data[adataimm : adataimm+len(xyd)]
         print(getdata)
         f.close()
         break
 
    if help.lower() =="nem":
+    print("Program vége!!")
     break
    
    else:
     i += 1
+
+   
+       
+
+#Nev -->  F F ---> https://www.facebook.com/profile.php?id=100082492849171
